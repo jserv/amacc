a04e310c35ca364618953f57e8c82dc99d480654
Jim Huang <jserv.tw@gmail.com>
2016-02-03 20:35:39 +0800

    tests: add 'switch-case'

---
+++ tests/switch.c
@@ -0,0 +1,11 @@
+int main(int argc, char **argv)
+{
+    switch (argc) {
+    case 1: printf("No arguments\n"); return 0;
+    case 2: printf("arg = %s\n", argv[1]); break;
+    default:
+        printf("More than 1 argument\n");
+        break;
+    }                                                                                                                       
+    return 0;
+}
