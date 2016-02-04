c49a264c2ee9f641f9a18ad17941249685770c74
Jim Huang <jserv.tw@gmail.com>
2016-02-04 18:59:05 +0800

    tests: simplify read case

--- tests/read.c
+++ tests/read.c
@@ -5,12 +5,10 @@ int main(int argc, char **argv)
 {
     char *p;
     int fd, readsz;
-    ++argv;
 
     if (!(p = malloc(1024 * 16))) { printf("failed to malloc memory\n"); exit(1); }
 
-    printf("opening %s...\n", *argv);
-    fd = open(*argv, 0);
+    fd = open("amacc.c", 0);
 
     readsz = read(fd, p, 1024 * 16);
     printf("read %d bytes\nContents:\n______________________________\n%s", readsz, p);
