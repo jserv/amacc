71a40cb4433440ae0e9906dc31e9911bd3473c9e
Jim Huang <jserv.tw@gmail.com>
2016-03-18 20:16:29 +0800

    tests: add 'struct'

---
+++ tests/struct.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+struct foo {
+    char *p;
+    char c;
+    char *pad;
+    int x;
+};
+
+int main(int argc, char **argv)
+{
+    struct foo bar, *p;
+    bar.x = 1;
+    p = &bar;
+
+    // FIXME: 32-bit only
+    if (*(int *)((void *)p + sizeof(struct foo) - 4) != bar.x)
+        return -1;
+
+    printf("%d\n", sizeof(struct foo));
+    return 0;
+}
