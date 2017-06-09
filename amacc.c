041a72ba56114223a57bcefb8fbc8fdde2378c22
LFChang <riversnow@gmail.com>
2017-06-09 16:42:21 +0800

    Fix pointer arithmetic error by shift operation
    Fixes #21
    
    The original manipulation to subtract the offset by ptr size, which involves in division by ptr size.
    At present, no DIV-like instruction is implemented, and this patch replaces with shit operations.
    
    * fix switch indent style
    
    modify switch indent to use same style
    
    * Update amacc.c
    
    Add indent in switch
    
    * Update amacc.c
    
    thanks, I did not notice that

--- amacc.c
+++ amacc.c
@@ -408,7 +408,10 @@ void expr(int lev)
             sz = t >= PTR2 ? sizeof(int) :
                              t >= PTR ? tsize[t - PTR] : 1;
             if (t == ty && sz > 1) {
-                *++e = SUB; *++e = PSH; *++e = IMM; *++e = sz; *++e = SUB;
+                switch (sz) {
+                case 4: *++e = SUB; *++e = PSH; *++e = IMM; *++e = 2; *++e = SHR; break;
+                default: *++e = SUB; *++e = PSH; *++e = IMM; *++e = sz; *++e = SUB; break;
+                }
                 ty = INT;
             } else if (sz > 1) {
                 *++e = PSH; *++e = IMM; *++e = sz; *++e = MUL; *++e = SUB;
