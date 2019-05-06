eb9bd8e287e57985683b3ea4c88b9aea2abad1b5
splasky <henrychung860326@gmail.com>
2019-05-06 12:34:27 +0800

    Update test files
    This commit move the struct local declaration from `local.c`
    into `struct.c` test file.

--- tests/local.c
+++ tests/local.c
@@ -1,21 +1,13 @@
-struct Value {
-    int r;
-    int i;
-};
-
 int main(void)
 {
     int n = 10;
-    int *c = &n;
     printf("%d\n", n);
-    printf("%d\n", *c);
     char cc = 'a';
     char *ptr = &cc;
     printf("%c\n", *ptr);
-    struct Value a;
-    a.r = 10;
-    a.i = 20;
-    struct Value *v = &a;
-    printf("%d %d", v->r, v->i);
-    return 0;
+    int *c = &n;
+    printf("%d\n", *c);
+    int r;
+    r = 0;
+    return r;
 }
--- tests/struct.c
+++ tests/struct.c
@@ -2,29 +2,27 @@
 #include <stdlib.h>
 int len;
 struct foo {
-    char *p;
+    char* p;
     char c;
-    char *pad;
+    char* pad;
     int x;
 } * d;
 
-char *str;
+char* str;
 
-int main(int argc, char **argv)
+int main(int argc, char** argv)
 {
-    struct foo bar, *p;
-    struct foo *ptr;
-    char c;
+    struct foo bar, *ptr;
+    char c = '1';
     int i;
     str = "I am a String!\n";
     len = 10;
     bar.x = 1;
-    p = &bar;
+    struct foo* p = &bar;
     p->c = 'a';
-    c = '1';
 
     // FIXME: 32-bit only
-    if (*(int *) ((void *) p + sizeof(struct foo) - 4) != bar.x)
+    if (*(int*)((void*)p + sizeof(struct foo) - 4) != bar.x)
         exit(-1);
 
     printf("%zu\n", sizeof(struct foo));
@@ -33,14 +31,15 @@ int main(int argc, char **argv)
     d = malloc(sizeof(struct foo) * len);
     ptr = d;
     for (i = 0; i < len; ++i) {
-        d->p = "one";
-        d->pad = str;
-        d->x = i;
-        d->c = c;
-        ++d;
+        ptr->p = "one";
+        ptr->pad = str;
+        ptr->x = i;
+        ptr->c = c;
+        ++ptr;
         ++c;
     }
 
+    ptr = d;
     for (i = 0; i < len; ++i) {
         printf("%d------------\n", i);
         printf("%s\n", ptr->p);
@@ -50,5 +49,17 @@ int main(int argc, char **argv)
         printf("--------------\n");
         ++ptr;
     }
+
+    ptr = d;
+    for (i = 0; i < len; ++i) {
+        printf("%d------------\n", i);
+        printf("%s\n", (*ptr).p);
+        printf("%s\n", (*ptr).pad);
+        printf("%d\n", (*ptr).x);
+        printf("%c\n", (*ptr).c);
+        printf("--------------\n");
+        ++ptr;
+    }
+
     return 0;
 }
