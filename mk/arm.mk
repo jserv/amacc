b08d6f50efb3f52c3ab44a1fbc4deef168e0f07e
Jim Huang <jserv@biilabs.io>
2020-06-21 03:17:31 +0800

    Fix breakage with packaged GNU toolchain

--- mk/arm.mk
+++ mk/arm.mk
@@ -19,7 +19,7 @@ endif
 
 # FIXME: check ld-linux.so as well
 ARM_LD_LINUX_PATH := $(shell cd $(shell $(ARM_CC) --print-sysroot) 2>/dev/null && pwd)
-ifndef ARM_LD_LINUX_PATH
+ifeq ("$(ARM_LD_LINUX_PATH)","/") # packaged GNU toolchain
   ARM_LD_LINUX_PATH := $(shell dirname "$(shell which $(ARM_CC))")/..
   ARM_LD_LINUX_PATH := $(shell cd $(ARM_LD_LINUX_PATH) 2>/dev/null && pwd)
   ARM_LD_LINUX_PATH := $(ARM_LD_LINUX_PATH)/$(shell echo $(CROSS_COMPILE) | sed s'/.$$//')/libc
