4ae5e4a93e24af838a291f9b43ac16426eba2644
HPCguy <80992404+HPCguy@users.noreply.github.com>
2021-09-29 22:57:05 -0700

    Improve loop support: do-while, break, continue (#68)
    This patch improves the support of loops:
    1.  Add do-while
    2. Add support for break/continue in the context of loops: AMaCC now
        raises compiler warning for use of break, case, default, continue
        outside of proper context.

--- tests/while.c
+++ tests/while.c
@@ -12,8 +12,22 @@ int fact(int n)
     return r;
 }
 
+int filteradd(char *data)
+{
+    int sum = 0;
+    do {
+        if (*data == '*') break;
+        if (*data < '0' || *data > '9') continue;
+        sum += *data - '0';
+    } while(*++data != 0);
+
+    return sum;
+}
+
 int main(int argc, char **argv)
 {
     printf("%d\n", fact(8));
+    printf("\n%d\n", filteradd("445h5h5g*45hb7b4g5"));
+
     return 0;
 }
