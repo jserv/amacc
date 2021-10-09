d767d3147806194452bf587c71a32fb47dcb7210
HPCguy <80992404+HPCguy@users.noreply.github.com>
2021-10-09 04:24:34 -0700

    Optimize power of 2 Div, Mod, and Mul (#69)

--- amacc.c
+++ amacc.c
@@ -457,6 +457,16 @@ void next()
 
 char fatal(char *msg) { printf("%d: %s\n", line, msg); exit(-1); }
 
+int popcount32b(int xx)
+{
+    xx -= xx >> 1 & 0x55555555;
+    xx = (xx >> 2 & 0x33333333) + (xx & 0x33333333);
+    xx = (xx >> 4) + (xx & 0x0f0f0f0f);
+    n += xx >> 8;
+    n += xx >> 16;
+    return (xx & 0x1f) ;
+}
+
 /* expression parsing
  * lev represents an operator.
  * because each operator `token` is arranged in order of priority, so
@@ -808,7 +818,13 @@ void expr(int lev)
         case Mul:
             next(); expr(Inc);
             if (*n == Num && *b == Num) n[1] *= b[1];
-            else { *--n = (int) b; *--n = Mul; }
+            else {
+                *--n = (int) b;
+                if (n[1] == Num && n[2] > 0 && (n[2] & (n[2] - 1)) == 0) {
+                    n[2] = popcount32b(n[2] - 1); *--n = Shl; // 2^n
+                }
+                else *--n = Mul;
+            }
             ty = INT;
             break;
         case Inc:
@@ -823,13 +839,25 @@ void expr(int lev)
         case Div:
             next(); expr(Inc);
             if (*n == Num && *b == Num) n[1] /= b[1];
-            else { *--n = (int) b; *--n = Div; }
+            else {
+                *--n = (int) b;
+                if (n[1] == Num && n[2] > 0 && (n[2] & (n[2] - 1)) == 0) {
+                    n[2] = popcount32b(n[2] - 1); *--n = Shr; // 2^n
+                }
+                else *--n = Div;
+            }
             ty = INT;
             break;
         case Mod:
             next(); expr(Inc);
             if (*n == Num && *b == Num) n[1] %= b[1];
-            else { *--n = (int) b; *--n = Mod; }
+            else {
+                *--n = (int) b;
+                if (n[1] == Num && n[2] > 0 && (n[2] & (n[2] - 1)) == 0) {
+                    --n[2]; *--n = And; // 2^n
+                }
+                else *--n = Mod;
+            }
             ty = INT;
             break;
         case Dot:
